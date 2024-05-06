6b5f8cd96c7b8b6518f7400628945ae500b4aca3
devMonkey87 <jlandetemunoz@icloud.com>
2024-05-06 20:10:27 +0200

    fix import

--- src/main/kotlin/com/example/models/Customer.kt
+++ src/main/kotlin/com/example/models/Customer.kt
@@ -2,15 +2,14 @@ package com.example.models
 
 import com.example.utils.EmailUtils
 import kotlinx.serialization.Serializable
-import java.util.*
+import java.util.UUID
 
 @Serializable
 data class Customer(
     val id: String = UUID.randomUUID().toString(),
     val firstName: String,
     val lastName: String?,
-    val email: String = EmailUtils().randomEmail()
+    val email: String = EmailUtils().randomEmail(),
 )
-
 
 val customerStorage = mutableListOf<Customer>()
